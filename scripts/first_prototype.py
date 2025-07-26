5143a0e075f9d23f0c4b7c64f2beff7f08d656dd
bitcraftmap <bitcraftmap@emailproxy.org>
2025-07-27 00:45:38 +0200

    the MAP

--- scripts/first_prototype.py
+++ scripts/first_prototype.py
@@ -1,81 +1,106 @@
-import matplotlib.pyplot as plt
+import json
 import numpy as np
+import matplotlib.pyplot as plt
 from matplotlib.patches import RegularPolygon
-import matplotlib.colors as mcolors
 
-n_hex_x = 32
-n_hex_y = 32
-hex_radius = 1  # size of hexagon
+def generate_one_chunk_image(json_data, options, color_map):
 
-# Pointy-topped hexagon dimensions
-hex_height = 2 * hex_radius
-hex_width = np.sqrt(3) * hex_radius
+    fig, ax = plt.subplots(figsize=(16, 16))
+    array_32_32 = [json_data['biomes'][i*32:(i+1)*32] for i in range(32)]
+    values = np.array(array_32_32)
+    
+    for row in range(options['chunk_y']):
+        for col in range(options['chunk_x']):
+            
+            # Calculate hex center x, y for pointy-topped hexagons
+            x = col * options['hex_width']
+            y = row * (options['hex_height'] * 3/4)
+            if row % 2 == 1:
+                x += options['hex_width'] / 2
 
-fig, ax = plt.subplots(figsize=(8, 8))
+            val = values[row, col]
+            val = val if val in range(15) else 0
+            color = color_map[val]
 
-# Generate some sample values for each hex cell
+            hexagon = RegularPolygon(
+                (x, y),
+                numVertices = 6,
+                radius = options['hex_radius'],
+                orientation = 0,
+                facecolor = color,
+                edgecolor = 'none',
+                antialiased = False
+            )
+            ax.add_patch(hexagon)
 
+    # Place any text in the hex maybe useful later
+    # ax.text(x, y, str(values[row, col]), ha='center', va='center', fontsize=10)
 
-biomes  = [11,11,11,11,11,11,11,11,11,11,11,11,11,11,267,267,267,267,267,267,267,2817,2817,2817,2817,1,1,1,1,1,1,1,11,11,11,11,11,11,11,11,11,11,11,11,11,267,267,267,267,267,267,267,267,2817,2817,2817,2817,1,1,1,1,1,1,1,11,11,11,11,11,11,11,11,11,11,11,11,267,267,267,267,267,267,267,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,11,11,11,11,11,11,11,11,11,11,11,267,267,267,267,267,267,2817,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,11,11,11,11,11,11,11,11,11,11,11,267,267,267,267,267,267,267,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,11,11,11,11,11,11,11,11,11,11,267,267,267,267,267,267,267,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,11,11,11,11,11,11,11,11,267,267,267,267,267,267,267,267,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,11,11,11,11,11,11,11,267,267,267,267,267,267,267,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,11,11,11,11,11,11,11,267,267,267,267,267,267,267,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,11,11,11,11,11,11,267,267,267,267,267,267,267,267,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,11,11,11,267,267,267,267,267,267,267,267,267,267,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,11,267,267,267,267,267,267,267,267,2817,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,11,267,267,267,267,267,267,267,267,267,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,11,267,267,267,267,267,267,267,267,267,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,267,267,267,267,267,267,267,267,267,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,267,267,267,267,267,2817,2817,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,267,267,267,267,267,267,2817,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,267,267,267,267,267,2817,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,267,267,267,267,2817,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,267,267,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,267,267,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,267,267,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2817,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2817,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2817,2817,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,2817,2817,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
-biomes_simple = [1 if x == 2817 else 11 if x == 267 else x for x in biomes]
+    # Set limits to avoid trimming
+    ax.set_xlim(-options['hex_width'] / 2, options['hex_width'] * (options['chunk_x'] + 0.5))      # Math Sorcery
+    ax.set_ylim(-options['hex_radius'], options['hex_height'] * (options['chunk_y'] * 3/4 + 0.25)) # Math Sorcery
+    ax.set_aspect('equal')                                        # Makes the aspect ratio of height and width the same
+    ax.axis('off')                                                # Remove legend on the sides
 
+    chunk_filename = 'assets/maps/chunks/r8/chunk_' + str(json_data['chunk_x']) + '_' + str(json_data['chunk_z']) + '.png'
 
+    plt.savefig(
+        chunk_filename,
+        dpi = 150,
+        bbox_inches = 'tight',
+        pad_inches = 0,
+        transparent = True
+    )
+    plt.close(fig)
+    plt.close()
 
-biomes_32 = [biomes_simple[i*32:(i+1)*32] for i in range(32)]
-values = np.array(biomes_32)
+hex_radius = 1
+options = {
+    'chunk_y' : 32,
+    'chunk_x' : 32,
+    'hex_radius': hex_radius,
+    'hex_height': 2 * hex_radius,
+    'hex_width': np.sqrt(3) * hex_radius
+}
 
-hex_colors = [
-    "#FF0000",
-    "#767B6C",
-    "#767B6C",
-    "#B2C791", 
-    "#B2C791",
-    "#B2C791",
-    "#B2C791",
-    "#B2C791",
-    "#B2C791",
-    "#B2C791",
-    "#B2C791", 
-    "#B2C791",
-    "#B2C791",
-    "#B2C791",
-    "#B2C791",
-    "#B2C791"
+'''
+biomes_colors = {
+    '0'  : '#FF0000', # Dev
+    '1'  : '#404932', # Calm Forest
+    '2'  : '#53584b', # Pine Woods
+    '3'  : '#b1b1af', # Snowy Peaks
+    '4'  : '#616147', # Breezy Grasslands
+    '5'  : '#484133', # Autumn Forest
+    '6'  : '#615257', # Misty Tundra
+    '7'  : '#72674d', # Desert Wasteland
+    '8'  : '#354540', # Swamp
+    '9'  : '#4c4956', # Rocky Garden
+    '10' : '#282f41', # Open Ocean
+    '11' : '#656d53', # Safe Meadows
+    '12' : '#222222', # Cave
+    '13' : '#495943', # Jungle
+    '14' : '#473d4c', # Sapwoods
+}
+'''
+biomes_colors = [
+    '#FF0000', # Dev
+    '#404932', # Calm Forest
+    '#53584b', # Pine Woods
+    '#b1b1af', # Snowy Peaks
+    '#616147', # Breezy Grasslands
+    '#484133', # Autumn Forest
+    '#615257', # Misty Tundra
+    '#72674d', # Desert Wasteland
+    '#354540', # Swamp
+    '#4c4956', # Rocky Garden
+    '#282f41', # Open Ocean
+    '#656d53', # Safe Meadows
+    '#222222', # Cave
+    '#495943', # Jungle
+    '#473d4c' # Sapwoods
 ]
 
-
-for row in range(n_hex_y):
-    for col in range(n_hex_x):
-        
-        # Calculate hex center x, y for pointy-topped hexagons
-        x = col * hex_width
-        y = row * (hex_height * 3/4)
-        if row % 2 == 1:
-            x += hex_width / 2  # offset every other row horizontally for pointy-topped
-
-        val = values[row, col]
-        color = hex_colors[val]  # pick color from your hex array
-
-        # Draw hexagon (no rotation needed for pointy-topped)
-        hexagon = RegularPolygon(
-            (x, y),
-            numVertices = 6,
-            radius = hex_radius,
-            orientation = 0,
-            facecolor = color,
-            edgecolor = 'none',
-            antialiased = False
-        )
-        ax.add_patch(hexagon)
-
-        # Place value text inside hex
-        # ax.text(x, y, str(values[row, col]), ha='center', va='center', fontsize=10)
-
-# Set limits to avoid trimming
-ax.set_xlim(-hex_width / 2, hex_width * (n_hex_x + 0.5))
-ax.set_ylim(-hex_radius, hex_height * (n_hex_y * 3/4 + 0.25))
-
-ax.set_aspect('equal')
-ax.axis('off')
-
-plt.show()
+with open('assets/data/terrain_chunk_state/region_8/terrain_chunk_state_new.json', 'r', encoding='utf-8') as file:
+    data = json.load(file)
+    for chunk in data:
+       generate_one_chunk_image(chunk, options, biomes_colors)
--- scripts/slash_region_data.py
+++ scripts/slash_region_data.py
@@ -18,10 +18,10 @@
 
 import json
 
-chunk_data_file = 'assets/data/terrain_chunk_state/region_1/terrain_chunk_state.json'
-chunk_data_file_new = 'assets/data/terrain_chunk_state/region_1/terrain_chunk_state_new.json'
+chunk_data_file = 'assets/data/terrain_chunk_state/region_8/terrain_chunk_state.json'
+chunk_data_file_new = 'assets/data/terrain_chunk_state/region_8/terrain_chunk_state_new.json'
 
-remove_keys = ['elevations', 'water_levels', 'zoning_types', 'original_elevations', 'chunk_index']
+remove_keys = ['elevations', 'water_levels', 'zoning_types', 'original_elevations', 'chunk_index', 'dimension']
 
 def remove_keys_from_dict(d, keys_to_remove):
     if isinstance(d, dict):
